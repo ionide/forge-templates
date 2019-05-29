0db298de75878fe5f260e3982fea3fe485709ad4
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2019-05-29 17:33:46 +0200

    Rework templates for .Net Core/Standard

--- nancyselfhosted/App.fs
+++
@@ -1,9 +0,0 @@
-namespace <%= namespace %>
-    module App=
-    
-    open Nancy
-
-    type App() as this =
-        inherit NancyModule()
-        do
-            this.Get.["/"] <- fun _ -> "Hello World!" :> obj
---
+++ saturn/ApplicationName.fs
@@ -0,0 +1,35 @@
+﻿module <%= namespace %>
+
+open System.IO
+open System.Threading.Tasks
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open FSharp.Control.Tasks.V2
+open Giraffe
+open Saturn
+
+type Counter = {Value: int}
+
+let getInitCounter() : Task<Counter> = task { return { Value = 42 } }
+
+let webApp = router {
+    get "/api/init" (fun next ctx ->
+        task {
+            let! counter = getInitCounter()
+            return! json counter next ctx
+        })
+}
+
+let app = application {
+    url "http://localhost:8080/"
+    use_router webApp
+    memory_cache
+    use_gzip
+}
+
+run app
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+    0 // return an integer exit code
